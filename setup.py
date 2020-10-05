d9d4ff42b0e348e35cf7a0cd772051eac8a59989
Bailey Srimoungchanh <srimoungchanh.bailey@gmail.com>
2020-10-05 01:07:25 -0500

    Cookiecutter template added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Predict the scores for each team using other columns with regression models',
+    author='Bailey Srimoungchanh',
+    license='',
+)
